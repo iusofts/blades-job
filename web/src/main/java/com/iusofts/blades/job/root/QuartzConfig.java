65360b48c2dd396c0384e571056d636bf08b6433
Ivan Shen <iusoft@sina.com>
2018-12-18 22:52:34 +0800

    定义失败阈值

--- web/src/main/java/com/iusofts/blades/job/root/QuartzConfig.java
+++ web/src/main/java/com/iusofts/blades/job/root/QuartzConfig.java
@@ -62,6 +62,7 @@ public class QuartzConfig {
         prop.put("org.quartz.jobStore.isClustered", "true");
         prop.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
         prop.put("org.quartz.threadPool.threadCount", "5");
+        prop.put("org.quartz.jobStore.misfireThreshold", "60000");
 
         prop.put("org.quartz.dataSource.quartzDataSource.driver", driver);
         prop.put("org.quartz.dataSource.quartzDataSource.URL", url);
